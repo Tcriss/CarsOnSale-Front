b503c635608c24d2a52bd0c8f04c7df19dd2b226
Cristian Tejeda <do.cristian.tr@hotmail.com>
2023-01-11 10:11:09 -0400

    fixes and implementations

--- CarsOnSale/src/app/components/login/login.component.ts
+++ CarsOnSale/src/app/components/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import { AlertsService } from 'src/app/services/alerts.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent {
   user: FormGroup;
 
-  constructor(private fb: FormBuilder){
+  constructor(
+    private fb: FormBuilder, 
+    private auth: AngularFireAuth, 
+    public alerts:AlertsService, 
+    private router:Router
+    ){
     this.user = this.fb.group({
       email: [''],
       password: ['']
@@ -19,5 +29,40 @@ export class LoginComponent {
   login(){
     const email = this.user.value.email;
     const password = this.user.value.password;
+
+    this.auth.signInWithEmailAndPassword(email,password)
+    .then(user => {
+      this.alerts.notifications('Welcome','','success');
+      this.router.navigate(['/home']);
+    })
+    .catch(error => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      this.alerts.notifications('Error',errorMessage,'error');
+    });
+  }
+
+  loginWithGoogle(){
+    this.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+    .then(user => {
+      this.alerts.notifications('Welcome','','success');
+      this.router.navigate(['/home']);
+    })
+    .catch(error => {
+      var errorCode = error.code;
+      let errorMessage = error.message;
+      this.alerts.notifications('Error',errorMessage,'error');
+    });
+  }
+
+  loginWithMicrosoft(){
+    this.auth.signInWithRedirect(new firebase.auth.OAuthProvider('microsoft.com'))
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      let errorMessage = error.message;
+      this.alerts.notifications('Error',errorMessage,'error');
+    });
   }
 }
